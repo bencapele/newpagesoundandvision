c304eb7f764505ea088a884ac1dd315e1bdc68ed
bencapele <78151015+bencapele@users.noreply.github.com>
2021-04-11 18:00:00 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+/* Contents Js */
+const ratio = 1
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: .1
+}
+
+const handleIntersect = function (entries, observer){
+  entries.forEach(function (entry) {
+    if (entry.intersectionRatio > ratio) {
+      entry.target.classList.add('reveal-visible')
+      observer.unobserve(entry.target)
+    } 
+  })
+}
+
+const observer = new IntersectionObserver(handleIntersect, options)
+document.querySelectorAll('[class*="reveal-"]').forEach(function (r){
+observer.observe()
+})
+
